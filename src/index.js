15ae57cf2f76fa850abaa2c033060bb1fdcd5b23
math45rosa-crypto <Matheus45rosa@gmail.com>
2025-11-21 20:45:33 -0300

    finalizado

--- src/index.js
+++ src/index.js
@@ -4,13 +4,26 @@ const connectDB = require('./config/database');
 
 const app = express();
 
-// Middleware para interpretar JSON
+
 app.use(express.json());
 
-// Conectar ao banco de dados
-connectDB();
 
-// Rotas da aplicação
+require('dotenv').config();
+const mongoose = require('mongoose');
+
+const { DB_USER, DB_PASS, DB_HOST, DB_NAME } = process.env;
+
+const url = `mongodb+srv://${DB_USER}:${DB_PASS}@${DB_HOST}/${DB_NAME}?retryWrites=true&w=majority`;
+
+mongoose.connect(url)
+  .then(() => {
+    console.log(" Conectado ao MongoDB com sucesso!");
+  })
+  .catch((err) => {
+    console.error(" Erro ao conectar ao MongoDB:", err.message);
+  });
+
+
 app.use('/alunos', require('./routes/alunoRoutes'));
 app.use('/professores', require('./routes/professorRoutes'));
 app.use('/departamentos', require('./routes/departamentoRoutes'));
@@ -22,12 +35,12 @@ app.use('/avaliacoes', require('./routes/avaliacaoRoutes'));
 app.use('/eventos', require('./routes/eventoRoutes'));
 app.use('/usuarios', require('./routes/usuarioRoutes'));
 
-// Rota simples para teste
+
 app.get('/', (req, res) => {
   res.send('API rodando corretamente!');
 });
 
-// Porta do servidor
+
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
